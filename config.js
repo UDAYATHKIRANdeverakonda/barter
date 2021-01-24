736e27dd45fc15b0803274a410e75f5011a0fc92
udu <70276732+UDAYATHKIRANdeverakonda@users.noreply.github.com>
2021-01-24 11:36:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmHW5ZQYxGIzuOW5PSj5yji5qGRlXgI1c",
+    authDomain: "book-santa-1fa62.firebaseapp.com",
+    projectId: "book-santa-1fa62",
+    storageBucket: "book-santa-1fa62.appspot.com",
+    messagingSenderId: "738535586235",
+    appId: "1:738535586235:web:5a98a938db04f03b15ec9e"
+  };
+
+  //Initialze Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
